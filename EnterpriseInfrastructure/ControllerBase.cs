ab93ef189fdda89dda6caa306f4286625b954ac0
icodingforfood <iprogrammer@live.cn>
2016-09-23 23:36:04 +0800

    continue to build infrastructure

---
+++ EnterpriseInfrastructure/ControllerBase.cs
@@ -0,0 +1,28 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace EnterpriseInfrastructure
+{
+    public class ControllerBase : IController
+    {
+        private readonly string _registeredRegionName;
+
+        public ControllerBase()
+        {
+
+        }
+
+        public ControllerBase(string registeredRegionName)
+            : this()
+        {
+            this._registeredRegionName = registeredRegionName;
+        }
+
+        public void Initialize()
+        {
+        }
+    }
+}
